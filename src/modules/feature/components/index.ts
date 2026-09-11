3f58b5b777a2f78d55211a90a1f6e2c5d59bd862
Lê Hoàng Khánh Duy <lhkhanhduy.dev@gmail.com>
2026-09-11 09:23:48 +0700

    fix: path

--- src/modules/feature/components/index.ts
+++ src/modules/feature/components/index.ts
@@ -1,5 +1,5 @@
-export { default as IntegrationFeatureSection } from './IntegrationFeatureSection';
-export { default as IntegrationContent } from './IntegrationContent';
-export { default as OrbitalMap } from './OrbitalMap';
-export { default as CentralHub } from './CentralHub';
-export { default as OrbitalNode } from './OrbitalNode';
+export { default as IntegrationFeatureSection } from '@/modules/feature/components/IntegrationFeatureSection';
+export { default as IntegrationContent } from '@/modules/feature/components/IntegrationContent';
+export { default as OrbitalMap } from '@/modules/feature/components/OrbitalMap';
+export { default as CentralHub } from '@/modules/feature/components/CentralHub';
+export { default as OrbitalNode } from '@/modules/feature/components/OrbitalNode';
